de8f0275a168545369236735626900406990c6aa
Beasley <beasley.com@gmail.com>
2021-04-05 12:34:44 -0700

    basic api implementation --> no auth

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const getEvents = require('./src/getEvents.js');
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/bananas', async (req, res) => {
-    let results = await getEvents('beasleydotcom')
-    console.log('this is server side results: 66666666666666666666666666666666666666666666666666666:       ', results)
+app.get('/api/v1/', async (req, res) => {
+    console.log('req.query', req.query.username);
+
+    let results = await getEvents(req.query.username);
+    // console.log('this is server side results: 66666666666666666666666666666666666666666666666666666:       ', results)
     res.send(results);
 })
 
